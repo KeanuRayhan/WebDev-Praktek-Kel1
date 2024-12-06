b960ae7a54d850f7269b5640133eb7d7570ffd16
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2024-12-06 21:23:42 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000, https://dramaku-kel1.vercel.app/"]
 };
 
 app.use(cors(corsOptions));
