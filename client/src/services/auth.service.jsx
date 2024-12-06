3180a5d67e6b90ffd48b47b87df5d4b6a48b0ddc
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2024-12-06 22:52:31 +0700

    Update auth.service.jsx

--- client/src/services/auth.service.jsx
+++ client/src/services/auth.service.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "https://webdev-praktek-kel1-production.up.railway.app/api/auth/";
 
 const register = (username, email, password) => {
-    return axios.post(API_URL + "signup", {
+    axios.post(API_URL + "signup", {
         username,
         email,
         password,
@@ -11,7 +11,7 @@ const register = (username, email, password) => {
 };
 
 const login = (username, password) => {
-    return axios.post(API_URL + "signin", {
+    axios.post(API_URL + "signin", {
         username,
         password,
     })
@@ -25,7 +25,7 @@ const login = (username, password) => {
 };
 
 const googleLogin = (token) => {
-    return axios.post(API_URL + "google-login", { token }, { withCredentials: true })
+    axios.post(API_URL + "google-login", { token }, { withCredentials: true })
     .then((response) => {
         if (response.data.accessToken) {
             localStorage.setItem("user", JSON.stringify(response.data));
